076be176ed3796e7e7e7a9e0762a29270f81736f
Hyeon-mook Jerry Choi <chm073@gmail.com>
2016-03-14 00:06:52 +0900

    configure service

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 var app = angular.module('flapperNews', []);
 
 app.factory('posts', [function() {
-    //service body
+    var o = {
+        posts: []
+    };
+    return o;
 }])
 
 app.controller('MainCtrl', [
     '$scope',
-    function($scope){
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5},
-            {title: 'post 2', upvotes: 2},
-            {title: 'post 3', upvotes: 15},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ];
+    'posts',
+    function($scope, posts){
+        $scope.posts = posts.posts;
         $scope.test = 'Hello world!';
         $scope.addPost = function() {
             if(!$scope.title || $scope.title === '') { return; }
